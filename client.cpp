76fd51eadc85fbfa043dabe0d697df040cf6a675
Paul Barrett <pmbarrett314@gmail.com>
2015-03-21 18:28:07 -0500

    worked on actual server message

--- client.cpp
+++ client.cpp
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <w32api/winsock.h>
+#include <w32api/ws2tcpip.h>
 
 #include "CSE4153.h"
 
@@ -17,7 +19,7 @@ char page[2048];
 
 void parse_arguments_and_flags(int argc, char *argv[]);
 
-struct in_addr get_IP_from_args();
+void parseURL();
 
 void handle_ctrl_c();
 
@@ -25,20 +27,16 @@ void create_socket();
 
 void connect_to_server();
 
-void parseURL();
-
-void get_input_from_user(char *buffer);
-
-int send_message_to_server(char *buffer);
+int get_page_from_server();
 
 int main(int argc, char *argv[]) {
     parse_arguments_and_flags(argc, argv);
+    parseURL();
 
     handle_ctrl_c();
 
     printf("started with URL: %s ... \n", args[0]);
-    parseURL();
-    exit(EXIT_SUCCESS);
+
     create_socket();
 
     connect_to_server();
@@ -46,15 +44,13 @@ int main(int argc, char *argv[]) {
 
     int exitv = 0;
     do {
-        char buffer[BUFSIZ];
-        get_input_from_user(buffer);
 
-        exitv = send_message_to_server(buffer);
+        exitv = get_page_from_server();
         if (0 != exitv) {
             //get return from the server and print it
             char recvbuffer[BUFSIZ];
             recv(sock, recvbuffer, BUFSIZ, 0);
-            printf("server sent back: \"%s\"\n", recvbuffer);
+            printf("%s", recvbuffer);
         }
     } while (0 != exitv);
 
@@ -97,16 +93,12 @@ void parse_arguments_and_flags(int argc, char *argv[]) {
         }
 
     }
-
-
-
 }
 
 void parseURL() {
     size_t first_slash = strcspn(args[0], "/");
     strncpy(host, args[0],first_slash);
     strncpy(page, &args[0][first_slash], strlen(args[0]) - first_slash);
-    printf("%s\t\t%s", host, page);
 }
 
 void handler(int sig) {
@@ -145,66 +137,38 @@ void connect_to_server() {
 
     struct addrinfo hints;
     struct addrinfo *servinfo;
+    memset(&hints,0,sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype=SOCK_STREAM;
+    hints.ai_flags= AI_PASSIVE;
 
     int status;
-    if ((status = getaddrinfo(args[0], port, &hints, &servinfo)) != 0) {
+    if ((status = getaddrinfo(host, port, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    sockaddr_in serveraddr;
 
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    //serveraddr.sin_port = htons(port);
-    //serveraddr.sin_addr = serverIP;
 
-    if (-1 == connect(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr))) {
+    if (-1 == connect(sock, servinfo->ai_addr, servinfo->ai_addrlen)) {
         close(sock);
         fprintf(stderr, "error connect failed: %s serverIP: %s port: %d\n", strerror(errno), args[0], port);
         exit(EXIT_FAILURE);
     }
 
+    freeaddrinfo(servinfo);
+
 }
 
-void get_input_from_user(char *buffer) {
-    //get input from commandline and check for ctrl+d
-    if (NULL == fgets(buffer, BUFSIZ, stdin)) {
-        if (!ferror(stdin)) {
-            buffer[0] = 4;
-            buffer[1] = '\0';
-        }
-        else {
-            perror("Input error : ");
-            close(sock);
-            exit(EXIT_FAILURE);
-        }
-    }
-}
-
-int send_message_to_server(char *buffer) {
+int get_page_from_server() {
     //checks the user input to see if it should quit
     //then sends the data
     //returns 1 if should continue, 0 if should quit
-    int retval;
-    if (strstr(buffer, "\x04\0") != NULL) {
-        printf("EOT entered. disconnecting...\n");
-        send(sock, buffer, strlen(buffer) + 1, 0);
-        retval = 0;
-    }
-    else if (strstr(buffer, "*QUIT*") != NULL) {
-        printf("*QUIT* entered. disconnecting and killing server...\n");
-        send(sock, buffer, strlen(buffer) + 1, 0);
-        retval = 0;
-    }
-    else {
-        //remove trailing newline and send data.
-        char *p;
-        if ((p = strchr(buffer, '\n')) != NULL) {
-            *p = '\0';
-        }
-        send(sock, buffer, strlen(buffer) + 1, 0);
-        retval = 1;
-    }
+    int retval=0;
+    char message[5012];
+    snprintf(message,5012,"GET %s HTTP/1.1\\r\\n Host: %s\\r\\n \\r\\n \\r\\n",page,host);
+
+    send(sock,message,5012,0);
+
     return retval;
 };
